a5a4cf5663d617e4ad4580eaa3557eca5f03c990
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-04 01:24:10 -0200

    adicionando componente calculadora

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+  	CalculadoraComponent
+  ]
 })
 export class CalculadoraModule { }
