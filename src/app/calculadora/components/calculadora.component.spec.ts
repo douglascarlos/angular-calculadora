6e7b795dd7db3f29405f6ec0cd9aced7b6faa699
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-05 01:03:08 -0200

    adicionando teste para soma no componente calculadora

--- src/app/calculadora/components/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CalculadoraComponent } from './calculadora.component';
 import { CalculadoraService } from '../services';
@@ -27,5 +28,27 @@ describe('CalculadoraComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve garantir que 5 + 8 = 13', () => {
+    let btn5 = fixture.debugElement.query(By.css('#btn5'));
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn8 = fixture.debugElement.query(By.css('#btn8'));
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btn5.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn8.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('13');
   });
 });
