99e357e1b1919c6bd4723d3cc45d081029590067
Douglas Carlos <dnevescarlos@gmail.com>
2018-01-04 22:25:54 -0200

    testando service calculadora

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -9,7 +9,46 @@ describe('CalculadoraService', () => {
     });
   });
 
-  it('should be created', inject([CalculadoraService], (service: CalculadoraService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', inject(
+  	[CalculadoraService], 
+  	(service: CalculadoraService) => {
+	    expect(service).toBeTruthy();
+	  }
+	));
+
+	it('deve garantir que 3 + 4 = 7', 
+		inject([CalculadoraService],(service: CalculadoraService) => {
+			let resultado = service.calcular(3, 4, CalculadoraService.SOMA);
+			expect(resultado).toEqual(7);
+		})
+	);
+
+	it('deve garantir que 3 - 4 = -1', 
+		inject([CalculadoraService],(service: CalculadoraService) => {
+			let resultado = service.calcular(3, 4, CalculadoraService.SUBTRACAO);
+			expect(resultado).toEqual(-1);
+		})
+	);
+
+	it('deve garantir que 3 / 4 = 0.75', 
+		inject([CalculadoraService],(service: CalculadoraService) => {
+			let resultado = service.calcular(3, 4, CalculadoraService.DIVISAO);
+			expect(resultado).toEqual(0.75);
+		})
+	);
+
+	it('deve garantir que 3 * 4 = 12', 
+		inject([CalculadoraService],(service: CalculadoraService) => {
+			let resultado = service.calcular(3, 4, CalculadoraService.MULTIPLICACAO);
+			expect(resultado).toEqual(12);
+		})
+	);
+
+	it('deve retornar 0 para operação inválida', 
+		inject([CalculadoraService],(service: CalculadoraService) => {
+			let resultado = service.calcular(3, 4, '^');
+			expect(resultado).toEqual(0);
+		})
+	);
+
 });
